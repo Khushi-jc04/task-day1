28612cf6f76de84a6e7e60be27fa98450ac7b6d1
Khushi <khushi@jcsoftwaresolution.com>
2025-10-31 18:52:35 +0530

    footer added

--- src/Days/Day-4/components/Footer.jsx
+++ src/Days/Day-4/components/Footer.jsx
@@ -1,17 +1,51 @@
+import "./footer.css"
 export function Footer() {
     return (
         <>
-            <div className="bg-green-800 text-white flex flex-gap-5">
+            <div className="bg-green-800 text-white flex md:gap-20 w-full">
                 <div>
                     <img src="https://res2.weblium.site/res/5f05a781c91c5b00229bcef4/5f1162dec3ffc600210d627c?nowebp" alt="image" className="lg:h-20 h-10" />
                 </div>
-                <div>
-                    About us
-                    Admissions
-                    Academics
+                <div class="lis">
+                    <ul>
+                        <li>
+                             About us
+                        </li>
+                        <li>
+                            Admissions
+                        </li>
+                        <li>
+                            Academics
+                        </li>
+                    </ul>
                 </div>
-                <div></div>
-                <div></div>
+                <div className="lis">
+                    <ul>
+                        <li>
+                            Campus Life
+                        </li>
+                        <li>
+                            News
+                        </li>
+                        <li>
+                        Contact us
+                        </li>
+                    </ul>
+                </div>
+                <div className="lis">
+                    <ul>
+                        <li>
+                             Our Teams
+                        </li>
+                        <li>
+                            Testimonals
+                        </li>
+                        <li>
+                            Events
+                        </li>
+                    </ul>
+                </div>
+                
             </div>
         </>
     )
